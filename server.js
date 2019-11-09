073206adc391e75d93d16c33bcf619fdda3f39cb
Istvan Acs <istvan.acs89@gmail.com>
2019-11-09 18:05:46 +0000

    Connecting to the database with mongoose

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // Load env variables
 dotenv.config({ path: './config/config.env' });
+
+// connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -19,7 +24,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
