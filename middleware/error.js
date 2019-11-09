976b673612f4e9d5362b03a96ce76f03a3631fa8
Istvan Acs <istvan.acs89@gmail.com>
2019-11-09 20:41:41 +0000

    Add custom ErrorResponse class

--- middleware/error.js
+++ middleware/error.js
@@ -2,7 +2,9 @@ const errorHandler = (err, req, res, next) => {
   // log to console for dev
   console.log(err.stack.red);
 
-  res.status(500).json({ success: false, error: err.message });
+  res
+    .status(err.statusCode || 500)
+    .json({ success: false, error: err.message || 'Server Error' });
 };
 
 module.exports = errorHandler;
