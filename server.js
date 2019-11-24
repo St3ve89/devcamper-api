43f0319a976dfa3b12526493f2eb8d2a93c57999
Istvan Acs <istvan.acs89@gmail.com>
2019-11-24 17:40:44 +0000

    Add photo uploaad for bootcamp functionality

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const path = require('path');
+const fileupload = require('express-fileupload');
 const erroHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -24,6 +26,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// File uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
